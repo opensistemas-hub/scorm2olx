2e4810a41d6e38af1bcea26aee0a7ceadc758f2d
Javi Muñoz <javimudi@users.noreply.github.com>
2017-10-26 15:51:13 +0200

    First skel for project

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='scorm2olx',
+      author='Javier Muñoz',
+      author_email='jmdiaz@opensistemas.com',
+      description='SCORM to Open Learning XML converter',
+      version=u'0.0.1',
+      packages=['scorm2olx'],
+      requires=['BeautifulSoup'],
+      url=''
+)
