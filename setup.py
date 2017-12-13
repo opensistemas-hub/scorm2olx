ca5b334162385a49a1e4e7f440423e4f2c89502c
Javi Muñoz <javimudi@users.noreply.github.com>
2017-12-13 12:55:40 +0100

    Dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,16 @@ setup(name='scorm2olx',
       description='SCORM to Open Learning XML converter',
       version=u'0.0.1',
       packages=['scorm2olx'],
-      install_requires=['beautifulsoup4', 'fs', 'scandir', 'Jinja2'],
+      install_requires=[
+            'appdirs==1.4.3',
+            'beautifulsoup4==4.6.0',
+            'enum34==1.1.6',
+            'fs==2.0.12',
+            'lxml==4.1.0',
+            'pytz==2017.2',
+            'scandir==1.6',
+            'six==1.11.0',
+            'jinja2'
+      ],
       url=''
 )
